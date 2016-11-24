079c92b35bbd4d2fc09cc7dc1724348664447a79
masterLB <look_hin11@hotmail.com>
2016-11-24 14:20:47 +0700

    Main

---
+++ app/src/main/java/rtc/kanyanat/chetsada/easyconvert/ConvertAActivity.java
@@ -0,0 +1,12 @@
+package rtc.kanyanat.chetsada.easyconvert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConvertAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
