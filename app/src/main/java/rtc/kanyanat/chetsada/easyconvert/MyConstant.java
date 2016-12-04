6581aaf5a7113da2e9e62f7d90a70f2774da61e4
masterLB <look_hin11@hotmail.com>
2016-12-04 10:36:13 +0700

    All Unit OK 2

--- app/src/main/java/rtc/kanyanat/chetsada/easyconvert/MyConstant.java
+++ app/src/main/java/rtc/kanyanat/chetsada/easyconvert/MyConstant.java
@@ -28,26 +28,35 @@ public class MyConstant {
 
     public String[] getLengthStrings() {
 
-        lengthStrings = new String[6];
+        lengthStrings = new String[10];
         lengthStrings[0] = "มม.";
         lengthStrings[1] = "ซม.";
         lengthStrings[2] = "ม.";
         lengthStrings[3] = "กม.";
         lengthStrings[4] = "นิ้ว";
         lengthStrings[5] = "หลา";
+        lengthStrings[6] = "ฟุต";
+        lengthStrings[7] = "ไมโคร.นิ้ว";
+        lengthStrings[8] = "ไมโคร.ม.";
+        lengthStrings[9] = "ไมล์";
+
 
         return lengthStrings;
     }
 
     public double[] getLengthDoubles() {
 
-        lengthDoubles = new double[6];
+        lengthDoubles = new double[10];
         lengthDoubles[0] = 1000;
         lengthDoubles[1] = 100;
         lengthDoubles[2] = 1;
         lengthDoubles[3] = 0.001;
         lengthDoubles[4] = 39.3433;
-        lengthDoubles[5] = 1.0936;
+        lengthDoubles[5] = 1.0936133;
+        lengthDoubles[6] = 3.2808399;
+        lengthDoubles[7] = 0.393700787;
+        lengthDoubles[8] = 1000000;
+        lengthDoubles[9] = 0.000621371192;
 
 
         return lengthDoubles;
@@ -75,7 +84,7 @@ public class MyConstant {
 
     public String[] getAreaStrings() {
 
-        areaStrings = new String[7];
+        areaStrings = new String[10];
         areaStrings[0] = "ตร.ม.";
         areaStrings[1] = "ตร.กม.";
         areaStrings[2] = "ตร.ซม.";
@@ -83,13 +92,18 @@ public class MyConstant {
         areaStrings[4] = "ตร.ไมล์";
         areaStrings[5] = "ตร.ฟุต";
         areaStrings[6] = "ตร.นิ้ว";
+        areaStrings[7] = "ตร.หลา";
+        areaStrings[8] = "ตร.ไมโครนิ้ว";
+        areaStrings[9] = "ตร.ไมโครเมตร";
+        areaStrings[10] = "เฮกตาร์";
+        areaStrings[11] = "เอเคอร์";
 
         return areaStrings;
     }
 
     public double[] getAreaDoubles() {
 
-        areaDoubles = new double[7];
+        areaDoubles = new double[12];
         areaDoubles[0] = 1;
         areaDoubles[1] = 0.000001000000;
         areaDoubles[2] = 10000.00;
@@ -97,58 +111,82 @@ public class MyConstant {
         areaDoubles[4] = 3.861022;
         areaDoubles[5] = 10.76391;
         areaDoubles[6] = 1550;
+        areaDoubles[7] = 1.195990046;
+        areaDoubles[8] = 0.015500031;
+        areaDoubles[9] = 1000000;
+        areaDoubles[10] = 0.0001;
+        areaDoubles[11] = 0.024710538;
+
 
         return areaDoubles;
     }
 
     public String[] getVolumeStrings() {
 
-        volumeStrings = new String[6];
+        volumeStrings = new String[13];
         volumeStrings[0] = "ลบ.ม.";
-        volumeStrings[1] = "ลบ.กม.";
+        volumeStrings[1] = "ลบ.นิ้ว.";
         volumeStrings[2] = "ลบ.ซล.";
         volumeStrings[3] = "ลบ.มม.";
-        volumeStrings[4] = "ล.";
-        volumeStrings[5] = "มล.";
+        volumeStrings[4] = "ลบ.หลา";
+        volumeStrings[5] = "ลบ.ฟุต";
+        volumeStrings[6] = "ลิตร";
+        volumeStrings[7] = "มล.";
+        volumeStrings[8] = "ซล.";
+        volumeStrings[9] = "ออนซ์";
+        volumeStrings[10] = "ไมโคร.ล.";
+        volumeStrings[11] = "กล.";
+        volumeStrings[12] = "ควอต";
 
         return volumeStrings;
     }
 
     public double[] getVolumeDoubles() {
 
-        volumeDoubles = new double[6];
+        volumeDoubles = new double[13];
         volumeDoubles[0] = 1;
-        volumeDoubles[1] = 1.000000;
+        volumeDoubles[1] = 61023.74;
         volumeDoubles[2] = 100000.0;
-        volumeDoubles[3] = 1.000000;
-        volumeDoubles[4] = 1000.000;
-        volumeDoubles[5] = 100000;
+        volumeDoubles[3] = 1000000000;
+        volumeDoubles[4] = 1.307951;
+        volumeDoubles[5] = 35.31467;
+        volumeDoubles[6] = 1000.000;
+        volumeDoubles[7] = 1000000;
+        volumeDoubles[8] = 100000.0;
+        volumeDoubles[9] = 35195.08;
+        volumeDoubles[10] = 1000000000;
+        volumeDoubles[11] = 1.000000;
+        volumeDoubles[12] = 879.88;
 
         return volumeDoubles;
     }
 
     public String[] getWeightStrings() {
 
-        weightStrings = new String[6];
+        weightStrings = new String[8];
         weightStrings[0] = "กก.";
         weightStrings[1] = "ก.";
         weightStrings[2] = "มก.";
         weightStrings[3] = "ตัน";
         weightStrings[4] = "ปอนด์";
         weightStrings[5] = "ออนซ์";
+        weightStrings[6] = "ไมโคร.ก.";
+        weightStrings[7] = "กะรัด";
 
         return weightStrings;
     }
 
     public double[] getWeightDoubles() {
 
-        weightDoubles = new double[6];
+        weightDoubles = new double[8];
         weightDoubles[0] = 1;
         weightDoubles[1] = 1000.000;
         weightDoubles[2] = 1000000;
         weightDoubles[3] = 0.00110231131;
         weightDoubles[4] = 2.20462262;
         weightDoubles[5] = 35.27396;
+        weightDoubles[6] = 1000000000;
+        weightDoubles[7] = 5000;
 
         return weightDoubles;
     }
